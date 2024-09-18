d62373e7fda75c6aba53516c1305ce9595450351
sapphire2207 <118914178+sapphire2207@users.noreply.github.com>
2024-09-18 11:37:58 +0530

    Add files via upload

---
+++ src/components/Tab2Container.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Tab2Container.css';
+
+const Tab2Container: React.FC = () => {
+  return (
+    <div className="hello-container">
+      <h1>Hello I'm Tab 2</h1>
+    </div>
+  );
+};
+
+export default Tab2Container;
---
+++ src/components/Tab3Container.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Tab3Container.css';
+
+const Tab3Container: React.FC = () => {
+  return (
+    <div className="hello-container">
+      <h1>Hello I'm Tab 3</h1>
+    </div>
+  );
+};
+
+export default Tab3Container;
